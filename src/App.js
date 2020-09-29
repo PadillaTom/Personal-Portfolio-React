2fb610fcaabd6417359810a47a531cdfb99cf966
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-29 19:19:23 +0200

    Hero Mobile Styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 // Components:
 import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* <Navbar Section */}
       <Navbar></Navbar>
       {/* Hero Section */}
+      <HeroSection></HeroSection>
     </div>
   );
 }
